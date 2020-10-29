bdf42feace80b8093f5ce7885db7bcb58f2e0c7d
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-10-29 12:59:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,23 +2,24 @@ sudo cp -rf transfer_learning_with_keras/docker-compose.yml .
 sudo cp -rf transfer_learning_with_keras/Dockerfile .
 sudo chmod -R 777 .
 
-#install Docker for Debian: https://docs.docker.com/install/linux/docker-ce/debian/
-sudo apt-get update
-sudo apt-get -y install \
+sudo apt-get -y update
+
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    gnupg2 \
+    gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install docker-ce docker-ce-cli containerd.io
 
-#fix Docker sock issue: https://stackoverflow.com/questions/47854463/got-permission-denied-while-trying-to-connect-to-the-docker-daemon-socket-at-uni
-sudo usermod -a -G docker $USER
-#have to restart after this
+sudo apt-get -y update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
