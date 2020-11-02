5d8f380977e3075c98a53b59f39e333b7bc290b8
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-11-02 13:56:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,5 +24,20 @@ sudo apt-get -y update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
+############### Install Docker Compose #####################
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+############## Install Nvidia-docker container GPU Support ################################# 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+   
+sudo apt-get update
+
+sudo apt-get install -y nvidia-docker2
+
+sudo systemctl restart docker
+
+sudo docker-compose up
+   
